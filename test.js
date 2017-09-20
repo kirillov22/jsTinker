003a377e6cb8cf1f8af9b74c102f9d662f7fee37
Kirill Kirillov <kki43@uclive.ac.nz>
2017-09-20 15:50:31 +1200

    Add button to generate a random int

--- test.js
+++ test.js
@@ -7,3 +7,10 @@ $('#counter').click(function() {
 
 
 var randInt = 0;
+var min = 0;
+var max = 1000;
+$('#randInt').click(function() {
+  randInt = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(randInt);
+  $('#txtRandomInt').text('Random integer: ' + randInt);
+});
